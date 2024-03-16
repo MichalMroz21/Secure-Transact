ff9178b2c5222fcd3b80e9d31ec12c46de8d163d
MichalMroz <125133223+MichalMroz21@users.noreply.github.com>
2024-03-16 23:00:56 +0100

    fixed .exe and package

--- source/main.cpp
+++ source/main.cpp
@@ -1,8 +1,8 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
-#include <gtest/gtest.h>
 #include <boost/algorithm/algorithm.hpp>
+#include <boost/algorithm/cxx11/is_sorted.hpp>
 
 #include <CMakeConfig.hpp>
 
@@ -11,6 +11,10 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
+
+    int c[] = {1,2,3,4};
+
+    bool ans = boost::algorithm::is_sorted(c);
 
     const QUrl url(u"qrc:/Task-Management-App/source_gui/Main.qml"_qs);
 
